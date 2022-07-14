6a6ce090f3a6515b4b45e5c49511b470877dcaea
Danial Ahmad <108906119+iamdanialahmad@users.noreply.github.com>
2022-07-14 12:25:21 +0500

    Update Exercise 2

--- Training/Week1/Exercise2.js
+++ Training/Week1/Exercise2.js
@@ -45,27 +45,3 @@ urls_array.populate();
 
 
 
-
-
-// Array.prototype.populate = function(){
-//     this.forEach((element,index)=> {
-//         new Promise((resolve,reject)=>{
-//             fetch(element).then(response=>{
-//                 if(response.ok){
-//                     console.log(response)
-//                     resolve(response.json())
-//                 }
-//                 else{
-//                     reject(response.status)
-//                 }
-//             }).then(res=> this[index] = res)
-//             .catch(err=>{
-//                 console.log(err)
-//             })
-//         })
-//     });
-// }
-// let arr_url = ['https://dummyjson.com/products/1', 'https://dummyjson.com/carts', 'https://dummyjson.com/users', 'https://dummyjson.com/posts', 'https://dummyjson.com/comments']
-
-// arr_url.populate()
-// console.log(arr_url)
